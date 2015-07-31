c548e247c0bab6d4aa898d44db3af4eeb931f015
Alex Budovski <alexbud@microsoft.com>
2015-07-31 00:01:41 -0700

    Improve deploy script.
    It now deploys both debug and release.

--- deploy.bat
+++ deploy.bat
@@ -1,10 +1,32 @@
 @echo off
+setlocal enabledelayedexpansion
+
 echo Deploying...
+
 if not exist deploy (
 	md deploy
 )
-xcopy /EYQ deps\python\runtime\lib deploy\Lib\
-copy deps\python\lib\*.dll deploy\
-copy deps\python\lib\*.pdb deploy\
-copy build\x64\debug\dbgscript.dll deploy\
-copy build\x64\debug\dbgscript.pdb deploy\
+
+set FLAVORS=debug release
+
+for %%f in (%FLAVORS%) do (
+    echo Deploying %%f
+    echo ---------------------
+    set OUTDIR=deploy\%%f
+    
+    if not exist !OUTDIR! (
+         md !OUTDIR!
+    )
+    
+    REM Copy over the Python standard library.
+    REM
+    xcopy /EYQ deps\python\runtime\lib deploy\%%f\Lib\
+    
+    REM Copy the Python DLLs
+    REM
+    copy deps\python\lib\*.dll deploy\%%f\
+    copy deps\python\lib\*.pdb deploy\%%f\
+    
+    copy build\x64\%%f\dbgscript.dll deploy\%%f\
+    copy build\x64\%%f\dbgscript.pdb deploy\%%f\
+)
